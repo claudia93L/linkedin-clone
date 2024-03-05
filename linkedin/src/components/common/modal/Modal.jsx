fdd10ad06abd7c1fa9f999f339c8fa415d93f8ac
claudia93L <34545518+claudia93L@users.noreply.github.com>
2024-03-05 10:30:23 +0100

    css modale

--- linkedin/src/components/common/modal/Modal.jsx
+++ linkedin/src/components/common/modal/Modal.jsx
@@ -16,7 +16,7 @@ const Modal = ({ open, onClose, children, title }) => {
           open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'
         }`}
       >
-        <div className='flex flex-row justify-between items-center border-b border-gray-400 p-2'>
+        <div className='flex flex-row justify-between items-center border-b border-gray-200 py-2 px-6'>
           <h1>{title}</h1>
           <button
             className=' p-1 rounded-lg text-gray-400 hover:bg.gray-50 hover: text-gray-600'
@@ -25,8 +25,10 @@ const Modal = ({ open, onClose, children, title }) => {
             <FontAwesomeIcon icon={faXmark}></FontAwesomeIcon>
           </button>
         </div>
-        <div className='flex flex-col p-6'>{children}</div>
-        <div className='flex flex-row justify-end border-t border-gray-400 px-6 py-3'>
+        <div className='flex flex-col p-6 text-gray-600 text-sm'>
+          {children}
+        </div>
+        <div className='flex flex-row justify-end border-t border-gray-200 px-6 py-3'>
           <Button className='bg-blue-600 py-1 px-4 text-white font-semibold rounded-full hover:bg-blue-800 cursor-pointer'>
             Save
           </Button>
